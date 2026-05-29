ee90da81c6f77c1fa98ee5d351775543d8d86f71
Andrey Dev <andreysamaniego1@gmail.com>
2026-05-29 17:11:54 -0400

    Load final responsive polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./mockup-fixes.css";
+import "./polish.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
